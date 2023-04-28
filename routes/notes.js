26cddc8d18b3949a9a9b8c9319a2a3aaac76f671
Garethus <reyes.grethelmaec@gmail.com>
2023-04-28 23:18:31 +1000

    linked front-end to back-end

---
+++ routes/notes.js
@@ -0,0 +1,37 @@
+const fb = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the feedback
+fb.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+// POST Route for submitting feedback
+fb.post('/', (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNotes = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNotes, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNotes,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting notes');
+  }
+});
+
+module.exports = fb;
